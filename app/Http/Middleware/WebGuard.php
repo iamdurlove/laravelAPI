e0bd2ce899a3b271ac016e359bd01cc3eee506df
Durlav Parajuli <durlove60@gmail.com>
2023-03-20 23:25:58 +0545

    middleware updated

---
+++ app/Http/Middleware/WebGuard.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class WebGuard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // echo $request->age;
+        if ($request->age > 18)
+            return $next($request);
+        else {
+            echo "access denied";
+            die;
+        }
+
+    }
+}
